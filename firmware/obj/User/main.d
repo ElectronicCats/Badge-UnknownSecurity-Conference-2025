341e8bac044868e92f44894ec9e66644d4b18249
deimos <deimoshall@gmail.com>
2025-06-10 09:30:08 -0600

    chore: rename project

---
+++ firmware/obj/User/main.d
@@ -0,0 +1,93 @@
+User/main.o: ../User/main.c ../User/../SSD1306/include/ssd1306.h \
+ ../User/../SSD1306/include/font_8x8.h ../User/bitmaps.h \
+ /home/mrsuser/dev/embedded/electronic_cats/Badge-UnknownSecurity-Conference-2025/firmware/Debug/debug.h \
+ /home/mrsuser/dev/embedded/electronic_cats/Badge-UnknownSecurity-Conference-2025/firmware/Peripheral/inc/ch32v00x.h \
+ /home/mrsuser/dev/embedded/electronic_cats/Badge-UnknownSecurity-Conference-2025/firmware/Core/core_riscv.h \
+ /home/mrsuser/dev/embedded/electronic_cats/Badge-UnknownSecurity-Conference-2025/firmware/User/system_ch32v00x.h \
+ /home/mrsuser/dev/embedded/electronic_cats/Badge-UnknownSecurity-Conference-2025/firmware/User/ch32v00x_conf.h \
+ /home/mrsuser/dev/embedded/electronic_cats/Badge-UnknownSecurity-Conference-2025/firmware/Peripheral/inc/ch32v00x_adc.h \
+ /home/mrsuser/dev/embedded/electronic_cats/Badge-UnknownSecurity-Conference-2025/firmware/Peripheral/inc/ch32v00x_dbgmcu.h \
+ /home/mrsuser/dev/embedded/electronic_cats/Badge-UnknownSecurity-Conference-2025/firmware/Peripheral/inc/ch32v00x_dma.h \
+ /home/mrsuser/dev/embedded/electronic_cats/Badge-UnknownSecurity-Conference-2025/firmware/Peripheral/inc/ch32v00x_exti.h \
+ /home/mrsuser/dev/embedded/electronic_cats/Badge-UnknownSecurity-Conference-2025/firmware/Peripheral/inc/ch32v00x_flash.h \
+ /home/mrsuser/dev/embedded/electronic_cats/Badge-UnknownSecurity-Conference-2025/firmware/Peripheral/inc/ch32v00x_gpio.h \
+ /home/mrsuser/dev/embedded/electronic_cats/Badge-UnknownSecurity-Conference-2025/firmware/Peripheral/inc/ch32v00x_i2c.h \
+ /home/mrsuser/dev/embedded/electronic_cats/Badge-UnknownSecurity-Conference-2025/firmware/User/ch32v00x_it.h \
+ /home/mrsuser/dev/embedded/electronic_cats/Badge-UnknownSecurity-Conference-2025/firmware/Peripheral/inc/ch32v00x_iwdg.h \
+ /home/mrsuser/dev/embedded/electronic_cats/Badge-UnknownSecurity-Conference-2025/firmware/Peripheral/inc/ch32v00x_misc.h \
+ /home/mrsuser/dev/embedded/electronic_cats/Badge-UnknownSecurity-Conference-2025/firmware/Peripheral/inc/ch32v00x_pwr.h \
+ /home/mrsuser/dev/embedded/electronic_cats/Badge-UnknownSecurity-Conference-2025/firmware/Peripheral/inc/ch32v00x_rcc.h \
+ /home/mrsuser/dev/embedded/electronic_cats/Badge-UnknownSecurity-Conference-2025/firmware/Peripheral/inc/ch32v00x_spi.h \
+ /home/mrsuser/dev/embedded/electronic_cats/Badge-UnknownSecurity-Conference-2025/firmware/Peripheral/inc/ch32v00x_tim.h \
+ /home/mrsuser/dev/embedded/electronic_cats/Badge-UnknownSecurity-Conference-2025/firmware/Peripheral/inc/ch32v00x_usart.h \
+ /home/mrsuser/dev/embedded/electronic_cats/Badge-UnknownSecurity-Conference-2025/firmware/Peripheral/inc/ch32v00x_wwdg.h \
+ /home/mrsuser/dev/embedded/electronic_cats/Badge-UnknownSecurity-Conference-2025/firmware/Peripheral/inc/ch32v00x_opa.h \
+ /home/mrsuser/dev/embedded/electronic_cats/Badge-UnknownSecurity-Conference-2025/firmware/Peripheral/inc/ch32v00x.h \
+ ../User/driver.h ../User/gpio.h ../User/oled_min.h ../User/i2c_tx.h \
+ ../User/hardware_tests.h ../User/menus.h ../User/tetris.h
+
+../User/../SSD1306/include/ssd1306.h:
+
+../User/../SSD1306/include/font_8x8.h:
+
+../User/bitmaps.h:
+
+/home/mrsuser/dev/embedded/electronic_cats/Badge-UnknownSecurity-Conference-2025/firmware/Debug/debug.h:
+
+/home/mrsuser/dev/embedded/electronic_cats/Badge-UnknownSecurity-Conference-2025/firmware/Peripheral/inc/ch32v00x.h:
+
+/home/mrsuser/dev/embedded/electronic_cats/Badge-UnknownSecurity-Conference-2025/firmware/Core/core_riscv.h:
+
+/home/mrsuser/dev/embedded/electronic_cats/Badge-UnknownSecurity-Conference-2025/firmware/User/system_ch32v00x.h:
+
+/home/mrsuser/dev/embedded/electronic_cats/Badge-UnknownSecurity-Conference-2025/firmware/User/ch32v00x_conf.h:
+
+/home/mrsuser/dev/embedded/electronic_cats/Badge-UnknownSecurity-Conference-2025/firmware/Peripheral/inc/ch32v00x_adc.h:
+
+/home/mrsuser/dev/embedded/electronic_cats/Badge-UnknownSecurity-Conference-2025/firmware/Peripheral/inc/ch32v00x_dbgmcu.h:
+
+/home/mrsuser/dev/embedded/electronic_cats/Badge-UnknownSecurity-Conference-2025/firmware/Peripheral/inc/ch32v00x_dma.h:
+
+/home/mrsuser/dev/embedded/electronic_cats/Badge-UnknownSecurity-Conference-2025/firmware/Peripheral/inc/ch32v00x_exti.h:
+
+/home/mrsuser/dev/embedded/electronic_cats/Badge-UnknownSecurity-Conference-2025/firmware/Peripheral/inc/ch32v00x_flash.h:
+
+/home/mrsuser/dev/embedded/electronic_cats/Badge-UnknownSecurity-Conference-2025/firmware/Peripheral/inc/ch32v00x_gpio.h:
+
+/home/mrsuser/dev/embedded/electronic_cats/Badge-UnknownSecurity-Conference-2025/firmware/Peripheral/inc/ch32v00x_i2c.h:
+
+/home/mrsuser/dev/embedded/electronic_cats/Badge-UnknownSecurity-Conference-2025/firmware/User/ch32v00x_it.h:
+
+/home/mrsuser/dev/embedded/electronic_cats/Badge-UnknownSecurity-Conference-2025/firmware/Peripheral/inc/ch32v00x_iwdg.h:
+
+/home/mrsuser/dev/embedded/electronic_cats/Badge-UnknownSecurity-Conference-2025/firmware/Peripheral/inc/ch32v00x_misc.h:
+
+/home/mrsuser/dev/embedded/electronic_cats/Badge-UnknownSecurity-Conference-2025/firmware/Peripheral/inc/ch32v00x_pwr.h:
+
+/home/mrsuser/dev/embedded/electronic_cats/Badge-UnknownSecurity-Conference-2025/firmware/Peripheral/inc/ch32v00x_rcc.h:
+
+/home/mrsuser/dev/embedded/electronic_cats/Badge-UnknownSecurity-Conference-2025/firmware/Peripheral/inc/ch32v00x_spi.h:
+
+/home/mrsuser/dev/embedded/electronic_cats/Badge-UnknownSecurity-Conference-2025/firmware/Peripheral/inc/ch32v00x_tim.h:
+
+/home/mrsuser/dev/embedded/electronic_cats/Badge-UnknownSecurity-Conference-2025/firmware/Peripheral/inc/ch32v00x_usart.h:
+
+/home/mrsuser/dev/embedded/electronic_cats/Badge-UnknownSecurity-Conference-2025/firmware/Peripheral/inc/ch32v00x_wwdg.h:
+
+/home/mrsuser/dev/embedded/electronic_cats/Badge-UnknownSecurity-Conference-2025/firmware/Peripheral/inc/ch32v00x_opa.h:
+
+/home/mrsuser/dev/embedded/electronic_cats/Badge-UnknownSecurity-Conference-2025/firmware/Peripheral/inc/ch32v00x.h:
+
+../User/driver.h:
+
+../User/gpio.h:
+
+../User/oled_min.h:
+
+../User/i2c_tx.h:
+
+../User/hardware_tests.h:
+
+../User/menus.h:
+
+../User/tetris.h:
